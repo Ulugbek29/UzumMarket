5b7b7ee4106bf8de48d8a16a6acb613169043a32
Ulugbek Ortigaliyev <ortigaliyevulugbek007@gmail.com>
2023-08-24 19:55:09 +0500

    Uzum market verstka

---
+++ swipper.js
@@ -0,0 +1,16 @@
+//    <!-- Initialize Swiper -->
+// Swipers sections
+
+
+// var swiper = new Swiper(".swiperSection", {
+//     cssMode: true,
+//     navigation: {
+//       nextEl: ".swiper-button-next",
+//       prevEl: ".swiper-button-prev",
+//     },
+//     pagination: {
+//       el: ".swiper-pagination",
+//     },
+//     mousewheel: true,
+//     keyboard: true,
+//   });
